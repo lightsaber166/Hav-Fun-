0ac4d5dff0046be6d073e3e63ce062c1dec1126e
lightsaber166 <142599447+lightsaber166@users.noreply.github.com>
2023-09-01 20:21:16 +0530

    Add files via upload

---
+++ bin/main.py
@@ -0,0 +1,1 @@
+#this is a set of assets, you can make your own code, do whatever you want here. Don't know python? watch this video for begginers: "https://www.youtube.com/watch?v=kqtD5dpn9C8". NOT MY VIDEO.
